be572bdbe07eb3b873b2038ca7a8967877abd425
bang-star <bang77060986@gmail.com>
2022-12-29 21:54:55 +0900

    #2 - SQLDelete 코드에 문제가 있어 Where 추가

--- src/main/java/com/example/model/entity/UserEntity.java
+++ src/main/java/com/example/model/entity/UserEntity.java
@@ -14,7 +14,7 @@ import java.time.Instant;
 @Getter
 @Entity
 @Table(name = "\"user\"")
-@SQLDelete(sql = "UPDATED \"user\" SET deleted_at = NOW() id=?")
+@SQLDelete(sql = "UPDATE \"user\" SET deleted_at = NOW() where id=?")
 @Where(clause = "deleted_at is NULL")
 public class UserEntity {
 
